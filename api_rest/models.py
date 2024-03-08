d45ae68c69b4bfd6a334f5e7d45423b01d17b396
Karen Dantas <karennobre74@gmail.com>
2024-03-08 15:26:44 -0300

    feat: create user model

--- api_rest/models.py
+++ api_rest/models.py
@@ -2,7 +2,10 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    user_nickname = models.CharField('nickname', max_length = 100, primary_key = True, default = '')
-    name = models.CharField('name', max_length = 100, default = ''),
-    email = models.EmailField('email', default = ''),
-    age = models.IntegerField('age', default = 0),
+    user_nickname = models.CharField('nickname', primary_key = True, max_length = 100, default = '')
+    name = models.CharField('name', max_length = 100, default = '')
+    email = models.EmailField('email', default = '')
+    age = models.IntegerField('age', default = 0)
+
+    def __str__(self):
+        return f'Nickname: {self.user_nickname}\nEmail:{self.email}'
