632699f5578f0dae35ac8fcb02827464bb91fc28
Karen Dantas <karennobre74@gmail.com>
2024-03-08 15:32:52 -0300

    register model in Admin

--- api_rest/models.py
+++ api_rest/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    user_nickname = models.CharField('nickname', primary_key = True, max_length = 100, default = '')
+    user_nickname = models.CharField(primary_key = True, max_length = 100, default = '')
     name = models.CharField('name', max_length = 100, default = '')
     email = models.EmailField('email', default = '')
     age = models.IntegerField('age', default = 0)
